3857f89c73629ed2ebda7dca2aae41ac9c43d2ce
Chitranshi26 <94301213+Chitranshi26@users.noreply.github.com>
2022-03-14 15:09:11 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,10 +2,10 @@
 public class App {
 public static void main(String[] args) {
 	System.out.println("Hello from git");
-<<<<<<< HEAD
+
 System.out.println("good bye");
-=======
+
 	System.out.println("Welcome to the GitHub");
->>>>>>> 69d9f3aa3d471b207ad04db359afb783b0a49cdb
+
 }
 }
